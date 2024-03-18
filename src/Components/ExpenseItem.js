7b3a21aa0cd8518c38fa5b47523069940bc57e37
Jonathan5511 <jonathanpereira5511@gmail.com>
2024-03-18 12:48:59 +0530

    Using JavaScript

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,12 +1,20 @@
-function ExpenseItem(){
-    return( <ul>
-        <li>Food - $5</li>
-        <li>Petrol - $20</li>
-        <li>Movie - $10</li>
-    </ul>
-    
-    );
-    
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  const expenseDate = new Date(2023, 7, 15).toISOString();
+  const expenseTitle = "Insurance";
+  const expenseAmount = 50;
+  const expenseLocation = "Bangalore";
+  return (
+    <div className="expense-item">
+      <div>{expenseDate}</div>
+      <div className="expense-item__location">{expenseLocation}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
