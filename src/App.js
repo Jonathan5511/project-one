316482f56318c80342ed0230e7ce02a9293e74c1
Jonathan5511 <jonathanpereira5511@gmail.com>
2024-03-18 17:54:15 +0530

    File Structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Expenses from "./Components/Expenses";
+import Expenses from "./Components/Expenses/Expenses";
 
-function App() {
+const App =() => {
   const expenses = [
     { id: "1", date: new Date(2023, 7, 15), title: "Insurance", price: 100 },
     { id: "2", date: new Date(2023, 3, 25), title: "Book", price: 10 },
