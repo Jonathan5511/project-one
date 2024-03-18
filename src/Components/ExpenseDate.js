7e34a284989c2a288f674c583d6e8748536f64ed
Jonathan5511 <jonathanpereira5511@gmail.com>
2024-03-18 16:44:13 +0530

    Splitting components

---
+++ src/Components/ExpenseDate.js
@@ -0,0 +1,31 @@
+import './ExpenseDate.css';
+
+const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+function ExpenseDate(props)
+{
+    const month = months[props.date.getMonth()];
+    const year = props.date.getFullYear();
+    const date = props.date.getDate();
+    return(
+        <div className="expense-date">
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{date}</div>
+      </div>
+    );
+}
+
+export default ExpenseDate;
