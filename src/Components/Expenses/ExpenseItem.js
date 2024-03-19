a05e7ce08574cbebcd15770fb13f56a63dbbe7a0
Jonathan5511 <jonathanpereira5511@gmail.com>
2024-03-19 10:15:28 +0530

    Delete Button

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -3,12 +3,14 @@ import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+
   return (
-    <Card className="expense-item">
+    <Card className="expense-item" id="item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.price}</div>
+        <button type="button">Delete</button>
       </div>
     </Card>
   );
