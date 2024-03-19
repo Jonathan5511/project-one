7716e0a019349faf4d01188f6bb2dac7dafdb274
Jonathan5511 <jonathanpereira5511@gmail.com>
2024-03-19 11:42:24 +0530

    Using State

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -1,16 +1,29 @@
 import './ExpenseItem.css';
 import ExpenseDate from "./ExpenseDate";
 import Card from '../UI/Card';
+import React,{useState} from 'react';
 
 const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title);
+  const [price, setPrice] = useState(props.price);
+
+  const onclickHandler = ()=>{
+    setTitle('updated!');
+    console.log(title);
+  }
+  const priceHandler = ()=>{
+    setPrice('100');
+  }
 
   return (
     <Card className="expense-item" id="item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.price}</div>
-        <button type="button">Delete</button>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${price}</div>
+        <button type="button" onClick={onclickHandler}>change title</button>
+        <button type="button" on onClick={priceHandler}>change price</button>
       </div>
     </Card>
   );
